37fe860be5bb5181ff053618b04b84ab007188c1
Daniel Etter <etter.dt@gmail.com>
2018-11-13 15:36:46 +0000

    automatically backed up by learn

---
+++ app/application.rb
@@ -0,0 +1,15 @@
+class Application
+  
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+    
+    if req.path=="/items"
+      item_name = req.path.split("/items/").last
+    else
+      resp.write "Route not found"
+      resp.status = 404
+    end
+  end
+  
+end
